050a809d936f434b49c29c09850329778e78bb04
naoki.tani <naoki.tani@mail.rakuten.com>
2014-01-06 23:33:14 +0900

    Add qt to tanihito.pp

--- modules/people/manifests/tanihito.pp
+++ modules/people/manifests/tanihito.pp
@@ -3,10 +3,14 @@ class people::tanihito {
   include caffeine
   include chrome
   include firefox
+  include gitx
   include iterm2::stable
+  include qt
   include skype
   include sublime_text_2
   include zsh
+
+  class { 'nodejs::global': version => 'v0.10' }
 
   # Ruby settings
   $ruby_version = '1.9.3'
--- modules/projects/manifests/work.pp
+++ modules/projects/manifests/work.pp
@@ -1,4 +1,10 @@
 class projects::work {
   include virtualbox
   include vagrant
+
+  package {
+  'Haskell':
+    source   => "http://download.binarynights.com/ForkLift2.5.4.zip",
+    provider => compressed_app;
+  }
 }
